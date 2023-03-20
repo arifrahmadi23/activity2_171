2b56d03bc2e3923e21df477f600b0f5ad6b574fe
arifrahmadi23 <muhammad231arif@gmail.com>
2023-03-20 14:53:32 +0700

    menambahkan fungsi dan membuat pilihan untuk memilih perhitungan luas persegi,lingkaran,segitiga, dan perintah exit

--- Latihan4_171/Latihan4_171.cpp
+++ Latihan4_171/Latihan4_171.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+using namespace std;
+
+float LuasPersegi(float p, float l) {
+	return p * l;
+}
+float LuasLingkaran(float r) {
+	return 3.14 * r * r;
+}
+float LuasSegitiga(float a, float t) {
+	return 0.5 * a * t;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	float panjang, lebar, jejari, alas, tinggi;
+	int pilihan;
+
+	do {
+		cout << "\n\n========================" << endl;
+		cout << "========= MENU =========" << endl;
+		cout << "========================" << endl;
+		cout << "1. Luas Persegi " << endl; 
+		cout << "2. Luas Lingkaran " << endl;
+		cout << "3. Luas Segitiga " << endl;
+		cout << "4. Exit " << endl;
+		cin >> pilihan;
+
+		switch (pilihan)
+		{
+		case 1: 
+
+		}
+
+
+	}
 }
